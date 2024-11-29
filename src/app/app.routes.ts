1c84a9ad978efb326cb3715240e2cb50c5e35505
Marcio Pezzi <marciopezzi92@gmail.com>
2024-11-29 17:27:59 -0300

    feat: add routing configuration for application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,32 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './auth/auth.guard';
+import { ClientsComponent } from './dashboard/views/clients/clients.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: LoginComponent 
-  }
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard', component: DashboardComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'clientes',
+        component: ClientsComponent,
+      },
+      {
+        path: 'clientes-selecionados',
+        component: ClientsComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'clientes',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  { path: '**', redirectTo: 'login' },
 ];
